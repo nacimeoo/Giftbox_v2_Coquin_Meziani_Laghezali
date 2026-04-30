efd8d8cdc957a8ec1337f92ec85a69b17b519e3b
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-04-30 10:56:26 +0200

    modif

--- gift.appli/src/models/Prestation.php
+++ gift.appli/src/models/Prestation.php
@@ -8,6 +8,7 @@ class Prestation extends Model
 {
     protected $table = 'prestation';
     protected $primaryKey = 'id';
+    public $incrementing = false;
     protected $keyType = 'string';
     public $timestamps = false;
 
@@ -15,4 +16,12 @@ class Prestation extends Model
         return $this->belongsTo(Categorie::class, 'cat_id');
     }
 
+    public function boxe() {
+        return $this->belongsToMany(Box::class, 'box2presta', 'presta_id', 'box_id')
+                    ->withPivot('quantite');
+    }
+
+    public function coffret() {
+        return $this->belongsToMany(CoffretType::class, 'coffret2presta', 'presta_id', 'coffret_id');
+    }
 }
