dd9cbe77a3a4622e85d9a2478194924df124004d
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-04-30 10:42:14 +0200

    ajout du model Categorie

---
+++ gift.appli/src/models/Categorie.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace gift\appli\src\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    protected $table = 'categorie';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+
+    public function prestations() {
+        return $this->hasMany(Prestation::class, 'cat_id');
+    }
+
+}
+
+
+ 
