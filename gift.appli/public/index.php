39ad5d8be8548478f1712d0a5cbc4849d73d8ae1
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-05-04 11:16:06 +0200

    modif

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -5,4 +5,19 @@ session_start();
 
 require_once __DIR__ . '/../src/vendor/autoload.php';
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$routesFile = __DIR__ . '/../src/routes.php';
+if (file_exists($routesFile)) {
+	require $routesFile;
+}
+
+
+$app->run();
+
 
