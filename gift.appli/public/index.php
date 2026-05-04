88254e6b45b6e3b62beb6133fdb65aadd802cce5
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-05-04 11:41:25 +0200

    exoo1

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -7,16 +7,12 @@ require_once __DIR__ . '/../src/vendor/autoload.php';
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
-use Slim\Factory\AppFactory;
-
-$app = AppFactory::create();
-
+$app = \Slim\Factory\AppFactory::create();
 $app->addRoutingMiddleware();
 $routesFile = __DIR__ . '/../src/conf/routes.php';
 if (file_exists($routesFile)) {
 	require $routesFile;
 }
-
 
 $app->run();
 
