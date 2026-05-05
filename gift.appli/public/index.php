53d93ad2a61c65d1958c6a12ebb36699d5d0e14a
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-05-05 17:51:03 +0200

    exo2

--- gift.appli/public/index.php
+++ gift.appli/public/index.php
@@ -7,6 +7,10 @@ require_once __DIR__ . '/../src/vendor/autoload.php';
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use gift\appli\utils\Eloquent;
+
+Eloquent::init(__DIR__ . '/../src/conf/gift.db.conf.ini');
+
 $app = \Slim\Factory\AppFactory::create();
 $app->addRoutingMiddleware();
 $app = (require_once __DIR__ . '/../src/conf/routes.php')($app);
--- gift.appli/src/controlers/GetPrestationAction.php
+++ gift.appli/src/controlers/GetPrestationAction.php
@@ -5,6 +5,7 @@ namespace gift\appli\controlers;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use gift\appli\models\Prestation;
 
 class GetPrestationAction extends AbstractAction {
     public function __invoke(Request $rq, Response $rs, array $args): Response {
@@ -15,29 +16,17 @@ class GetPrestationAction extends AbstractAction {
             return $rs->withStatus(400);
         }
         
-        $id = (int) $params['id'];
-        $prestations = [10 => ['title' => 'Massage détente', 'price' => '50€'], 11 => ['title' => 'Soin visage', 'price' => '40€']];
+        $id = $params['id'];
+        $prestation = Prestation::find($id);
         
-        if (!isset($prestations[$id])) {
+        if (!$prestation) {
             $rs->getBody()->write('<p>Prestation introuvable.</p>');
             return $rs->withStatus(404);
         }
         
-        $p = $prestations[$id];
-        $html = <<<HTML
-        <!DOCTYPE html>
-        <html lang="fr">
-        <head>
-            <meta charset="UTF-8">
-            <title>Prestation {$id}</title>
-        </head>
-        <body>
-            <h1>{$p['title']}</h1>
-            <p>Référence : {$id}</p>
-            <p>Prix : {$p['price']}</p>
-        </body>
-        </html>
-        HTML;
+        $html = "<h1>{$prestation->libelle}</h1>";
+        $html .= "<p>Description : {$prestation->description}</p>";
+        $html .= "<p>Tarif : {$prestation->tarif} €</p>";
 
         $rs->getBody()->write($html);
         return $rs;
