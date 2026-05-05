1121c9196e1b0aa7db26301a876093b5ad353b65
Arnaud <arnaud.coquin3@etu.univ-lorraine.fr>
2026-05-05 17:57:18 +0200

    ex3

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -2,11 +2,9 @@
 declare(strict_types=1);
 use gift\appli\utils\Eloquent;
 
-session_start();
-
 $app = \Slim\Factory\AppFactory::create();
 $app->addRoutingMiddleware();
 $app = (require_once __DIR__ . '/routes.php')($app);
-Eloquent::init(__DIR__ . '/../src/conf/gift.db.conf.ini');
+Eloquent::init(__DIR__ . '/gift.db.conf.ini');
 
 return $app;
