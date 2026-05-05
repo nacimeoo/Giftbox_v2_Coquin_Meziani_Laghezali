d0207a3e4b91d5afef4ce00d38281eb05202aa82
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-05-05 17:02:08 +0200

    routes

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-return function (Slim\App $app) {
-
-    $app->get('/categories', function (Request $rq, Response $rs, array $args): Response {
+return function (Slim\App $app): Slim\App {
+    $app->get('/categories', function (Request $request, Response $response, array $args): Response {
         $html = <<<HTML
         <!DOCTYPE html>
         <html lang="fr">
@@ -25,11 +24,72 @@ return function (Slim\App $app) {
         </html>
         HTML;
 
-        $rs->getBody()->write($html);
-        return $rs;
+        $response->getBody()->write($html);
+        return $response;
     });
 
-    
-    
+    $app->get('/categorie/{id}', function (Request $request, Response $response, array $args): Response {
+        $id = isset($args['id']) ? (int) $args['id'] : 0;
+        $label = null;
+        if ($id === 1) $label = 'Soins du visage';
+        if ($id === 2) $label = 'Massages';
+        if ($id === 3) $label = 'Coiffure';
+
+        if ($label === null) {
+            $response->getBody()->write('<p>Catégorie inconnue.</p>');
+            return $response->withStatus(404);
+        }
+
+        $html = <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>Catégorie {$id}</title>
+        </head>
+        <body>
+            <h1>Catégorie n°{$id}</h1>
+            <p>Libellé : {$label}</p>
+        </body>
+        </html>
+        HTML;
+
+        $response->getBody()->write($html);
+        return $response;
+    });
+
+    $app->get('/prestation', function (Request $request, Response $response, array $args): Response {
+        $params = $request->getQueryParams();
+        if (!isset($params['id']) || $params['id'] === '') {
+            $response->getBody()->write('<p>Paramètre manquant : id requis.</p>');
+            return $response->withStatus(400);
+        }
+        $id = (int) $params['id'];
+        $prestations = [10 => ['title' => 'Massage détente', 'price' => '50€'], 11 => ['title' => 'Soin visage', 'price' => '40€']];
+        if (!isset($prestations[$id])) {
+            $response->getBody()->write('<p>Prestation introuvable.</p>');
+            return $response->withStatus(404);
+        }
+        $p = $prestations[$id];
+        $html = <<<HTML
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>Prestation {$id}</title>
+        </head>
+        <body>
+            <h1>{$p['title']}</h1>
+            <p>Référence : {$id}</p>
+            <p>Prix : {$p['price']}</p>
+        </body>
+        </html>
+        HTML;
+
+        $response->getBody()->write($html);
+        return $response;
+    });
+
+    return $app;
 };
 
