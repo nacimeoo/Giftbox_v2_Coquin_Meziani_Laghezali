a9cc8ef94cf853f1f875f56b84f407099da11efc
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-05-06 15:44:07 +0200

    resolution probleme erreur

--- gift.appli/src/conf/routes.php
+++ gift.appli/src/conf/routes.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 return function (Slim\App $app): Slim\App {
     $app->get('/categories', \gift\appli\controlers\GetCategoriesAction::class);
-    $app->get('/categorie/{id}', \gift\appli\controlers\GetCategorieByIdAction::class);
+    $app->get('/categorie[/{id}]', \gift\appli\controlers\GetCategorieByIdAction::class);
     $app->get('/prestation', \gift\appli\controlers\GetPrestationAction::class);
 
     return $app;
