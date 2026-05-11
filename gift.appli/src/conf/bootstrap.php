79f1b55ec7f0b4743cc3ca8eeb191daa366c5116
Fahim Meziani <fahim.meziani5@etu.univ-lorraine.fr>
2026-05-11 08:25:52 +0200

    tp' ex1

--- gift.appli/src/conf/bootstrap.php
+++ gift.appli/src/conf/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 declare(strict_types=1);
 use gift\appli\utils\Eloquent;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 $app = \Slim\Factory\AppFactory::create();
+
+$twig = Twig::create(__DIR__ . '/../views', ['cache' =>'path/to/cache-dir', 'auto_reload' => true]);
+$app->add(TwigMiddleware::create($app, $twig));
+
 $app->addRoutingMiddleware();
 $app->addErrorMiddleware(true, false, false);
 $app = (require_once __DIR__ . '/routes.php')($app);
--- gift.appli/src/controlers/GetCategorieByIdAction.php
+++ gift.appli/src/controlers/GetCategorieByIdAction.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace gift\appli\controlers;
@@ -6,33 +7,31 @@ namespace gift\appli\controlers;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use gift\appli\models\Categorie;
+use Slim\Views\Twig;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpNotFoundException;
 use Slim\Exception\HttpInternalServerErrorException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 
-class GetCategorieByIdAction extends AbstractAction {
-    public function __invoke(Request $rq, Response $rs, array $args): Response {
-        if(!isset($args['id'])) {
-            throw new HttpBadRequestException($rq,"faut un id");
+class GetCategorieByIdAction extends AbstractAction
+{
+    public function __invoke(Request $rq, Response $rs, array $args): Response
+    {
+        if (!isset($args['id'])) {
+            throw new HttpBadRequestException($rq, "faut un id");
         }
         $id = (int)$args['id'];
-        
+
         try {
             $categorie = Categorie::findOrFail($id);
         } catch (ModelNotFoundException $e) {
             throw new HttpNotFoundException($rq, "introuvable");
-        }catch(QueryException $e){
+        } catch (QueryException $e) {
             throw new HttpInternalServerErrorException($rq, "erreur de bdd");
         }
-        
 
-        $html = "<h1>Catégorie n°{$categorie->id}</h1>";
-        $html .= "<p>Libellé : {$categorie->libelle}</p>";
-        $html .= "<p>Description : {$categorie->description}</p>";
-
-        $rs->getBody()->write($html);
-        return $rs;
+        $view = Twig::fromRequest($rq);
+        return $view->render($rs, 'categorie.twig', $categorie);
     }
 }
