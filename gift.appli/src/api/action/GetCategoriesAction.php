8b34320b5d34a353417c653e1fa515df7fd3092d
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2026-06-08 16:22:29 +0200

    api

---
+++ gift.appli/src/api/action/GetCategoriesAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace gift\appli\api\action;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+
+class GetCategoriesAction
+{
+    public function __invoke(Request $request, Response $response, array $args): Response
+    {
+        
+    }
+}
